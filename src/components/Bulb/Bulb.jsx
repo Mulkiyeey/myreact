eef4ae91e9ab5c3337bd61bce4fad1c69b80f3d2
Mulkiyeey <mulkiahmed@gmail.com>
2023-05-24 16:23:36 +0300

    first commit

---
+++ src/components/Bulb/Bulb.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import on from './on.png'
+import off from './off.png'
+const Bulb = () => {
+    const [toggle,setToggle] = useState(true)
+    const toggleBtn = () => {
+        setToggle(!toggle)
+    }
+  return (
+    <div>
+        <img src={toggle?on:off} width='200'
+        style={{
+          backgroundColor:'black',
+        }}/> <br/>
+        <button onClick={toggleBtn} style={{
+                padding: 5,
+                margin: 40,
+                width: 90,
+                // backgroundColor:'black',
+        }}>{toggle?'On':'Off'}</button>
+    </div>
+  )
+}
+
+export default Bulb
